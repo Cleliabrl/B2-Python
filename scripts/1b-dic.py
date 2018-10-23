e11c5c578a6651aeab6eb9d29dbf126439ee8d04
root <root@localhost.localdomain>
2018-10-23 19:04:30 +0200

    Affichage des prénom dans la liste (pas ordre alphabétique) A finir

--- scripts/1b-dic.py
+++ scripts/1b-dic.py
@@ -4,12 +4,16 @@
 # Afficher liste de prénom par ordre alphabétique :w
 
  
+ma_liste=[]
+saisie=0 
 
-
-
-
-
-
+while saisie != "q" : 
+	saisie = input("Entre un prénom : ")
+	ma_liste.append(saisie) 
+	print(ma_liste)
+ 
+#ma_liste.sort() 
+#ma_liste
 
 
  
