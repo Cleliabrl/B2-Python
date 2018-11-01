8094b79e32ba462c5906b06f8bb13caa61a2965a
root <root@localhost.localdomain>
2018-11-01 19:00:14 +0100

    Problème de dictionnaire

--- scripts/1c-moy.py
+++ scripts/1c-moy.py
@@ -4,6 +4,21 @@
 # Moyenne et top 5 de note   
 
 
+mon_dictionnaire = {} 
+saisie = 0 
+
+mon_dictionnaire["prénom"] = input("entre un prenom") 
+mon_dictionnaire["note"] = input("entre une note") 
+
+print (mon_dictionnaire) 
+#while (prenom =! q) : 
+#	prenom=input("Entre un prénom : ") 
+#	note=int(input("Entre une note : "))
+ 
+ 
+	 
+
+
 
 
 
