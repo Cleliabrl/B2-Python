f30a7c8f5fa95df19613b1e4aaf693f24b6a66be
root <root@localhost.localdomain>
2018-11-01 11:42:40 +0100

    regex à finir

--- scripts/1a-add.py
+++ scripts/1a-add.py
@@ -10,24 +10,34 @@ import re
 def Addition(nbr1,nbr2) :  #Fonction qui additionne les deux nombres saisies  
 	return int(nbr1) + int(nbr2)  
 
-def VerifSaisie():  #Fonction qui vérifie si la saisie nbr1 est un nombre
-	if regex.match(nbr1):
+def VerifSaisie(nbr1,nbr2):  #Fonction qui vérifie si la saisie nbr1 est un nombre
+	if nbr_regex.match(nbr1):
 		return True
 	else : 
 		print("La saisie n'est pas un nombre") 
-
-
+		exit()
+	if nbr_regex.match(nrb2) : 
+		return True 
+	else : 
+		print("La saisie n'est pas un nombre") 
+		exit()
 
 nbr1=0
 nbr2=0 
-regex=re.compile('^(([0-9])|(1[0-9])|100)$')
+nbr_regex = re.compile('^(([0-9])|(1[0-9])|100)$')
 
 
 
 nbr1=input("Entrez un nombre :") 
 nbr2=input("Entrez un deuxième nombre :") 
 
+if nbr_regex.match(nbr1) : 
+	print ("le nombre est entre 0 et 100") 
+else : 
+	print("le nombre n'est pas entre 0 et 100") 
 
+
+VerifSaisie(nbr1,nbr2) 
 
 result=Addition(nbr1, nbr2)  #appel de la fonction Addition() dans le code
 print(result)
