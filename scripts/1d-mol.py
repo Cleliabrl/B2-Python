1909c3252abb7655428c1b776e69701b97a3f5df
root <root@localhost.localdomain>
2018-11-06 17:23:32 +0100

    Verif saisie à finir

--- scripts/1d-mol.py
+++ scripts/1d-mol.py
@@ -8,31 +8,49 @@ import re
 
 
 nombrerand = random.randint(0,100)
-saisie = -1  
-print(nombrerand) 
-note_regex = re.compile('^(([0-9])|(1[9-0])|20)$') 
+print(nombrerand)
+saisie=input("Entre un nombre entre 0 et 100 : ")
+note_regex=re.compile('^(([0-9])|(1[0-9])|100)$') 
 
-def VerifSaisie(saisie): 
-	if(note-regex.match(note1):
-		saisie = int(saisie) 
-		if (saisie > 100):
-			return False 		
-		else : 
-			return True 
-	else : 
-		return False
 
-#def SaisieSortie(saisie): 
-#	if saisie)== q or saisie == Q) : 
-#		exit()  
 
-while (saisie != nombrerand):
-	while (VerifSaisie(saisie) == False) : 
-		saisie = input("ERREUR Saisir un nombre entre 1 et 100 : ") 
-	if (saisie ==  nombrerand):
-		print("Gagné") 
-	elif (saisie < nombrerand): 
+
+def Goodbay(): 
+	 print("Au revoir !  La réponse était : " + str(nombrerand))  
+
+
+#def VerifSaisie(saisie): 
+#	if (note_regex.match(saisie)):
+#		saisie = int(saisie)
+#		if (saisie > 100):
+#			return False
+#			print("Un nombre entre 0 ete 100 : " ) 		
+#		else : 
+#			return True 
+#	else : 
+#		return False
+
+
+def SaisieSortie(saisie): 
+	if (str(saisie) == q or str(saisie) == Q) : 
+		exit()  
+
+
+
+
+while (int(saisie) != nombrerand):
+	if (int(saisie) < nombrerand): 
 		print("C'est plus grand")
-	elif (saisie > nombrerand):
-		  print("C'est plus petit")  
-				
+		saisie = input("Entre un nouveau nombre : ")
+		SaisieSortie(saisie) 
+	elif (int(saisie) > nombrerand):
+		print("C'est plus petit")  
+		saisie = input("Etre un nouveau nombre : ")
+		SaisieSortie(saisie)  
+print("Gachné ! La réponse était : " + str(nombrerand))  
+
+
+
+
+
+ 
