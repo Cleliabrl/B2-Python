af1120247f9f10b42190d1e341ad479ddab1c0dd
root <root@localhost.localdomain>
2018-11-11 11:55:12 +0100

    Fini

--- scripts/1a-add.py
+++ scripts/1a-add.py
@@ -3,45 +3,41 @@
 # auteur : BURLE Clélia
 # Affiche le résultat d'une addition 
 
+
+#IMPORTS
+
 import re 
 
 
+#FONCTIONS
 
-def Addition(nbr1,nbr2) :  #Fonction qui additionne les deux nombres saisies  
+
+# fonction qui additionne les deux nombres
+def addition(nbr1,nbr2) : 
 	return int(nbr1) + int(nbr2)  
 
-def VerifSaisie(nbr1,nbr2):  #Fonction qui vérifie si la saisie nbr1 est un nombre
-	if nbr_regex.match(nbr1):
-		return True
-	else : 
-		print("La saisie n'est pas un nombre") 
-		exit()
-	if nbr_regex.match(nbr2): 
-		return True 
-	else : 
-		print("La saisie n'est pas un nombre") 
-		exit()
+
+# fonction qui vérifie la saisie
+def verifSaisie(nbr_regex): 
+	nb = input('Entre un nombre : ') 
+	while not nbr_regex.match(nb) : 
+		nb = input('Entre un vrai nombre : ') 
+	return int(nb) 
+
+
+#VARIABLES
 
 nbr1=0
 nbr2=0 
-nbr_regex = re.compile('^(([0-9])|(1[0-9])|100)$')
+nbr_regex = re.compile('^[0-9]+$') 
 
 
 
-nbr1=input("Entrez un nombre :") 
-nbr2=input("Entrez un deuxième nombre :") 
-
-#if nbr_regex.match(nbr):
-#	print ("le nombre est entre 0 et 100")
-#else : 
-#	print("le nombre n'est pas entre 0 et 100") 
-
-VerifSaisie(nbr1,nbr2) 
-
-result=Addition(nbr1, nbr2)  #appel de la fonction Addition() dans le code
-print(result)
+nbr1 = verifSaisie(nbr_regex)
+nbr2 = verifSaisie(nbr_regex) 
+ 
   
-
+print(addition(nbr1,nbr2)) 
 
 
 
